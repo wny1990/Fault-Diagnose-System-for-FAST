3faf92a92571509cc4462ff07866d11ddd37a4bf
wny <wny@localhost.localdomain>
2013-04-01 19:28:11 +0800

    v2,04/01/2013

--- mysite/mysite/views.py
+++ mysite/mysite/views.py
@@ -1,7 +1,16 @@
 from django.http import HttpResponse
+from django.shortcuts import render
+from django.shortcuts import render_to_response
+from django.template import Context, loader
 import datetime
 
 def homepage_view(request):
 	now = datetime.datetime.now()
-	html = "<html><body>It is now %s.</body></html>" % now
-	return HttpResponse(html)
+	context = Context({
+               'now': now,
+	})
+	return render(request,'homepage.html',context)
+	
+#def image(request,image_file):
+#    image_data = open("/home/wny/fd_sys/mysite/images/image_file", "rb").read()
+#    return HttpResponse(image_data, mimetype="image/png")
